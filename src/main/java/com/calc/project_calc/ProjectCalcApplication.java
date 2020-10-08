210a357a1b490a719ea65e29194607623bf207f0
Pragnya-Sahu <pragnya27@gmail.com>
2020-10-08 12:24:10 +0530

    adding project Calc

---
+++ src/main/java/com/calc/project_calc/ProjectCalcApplication.java
@@ -0,0 +1,13 @@
+package com.calc.project_calc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectCalcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectCalcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/calc/project_calc/ProjectCalcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calc.project_calc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectCalcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
