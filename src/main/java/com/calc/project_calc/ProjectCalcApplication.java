a18ceee67747b5be2611489852852a84e04991c5
Pragnya-Sahu <pragnya27@gmail.com>
2020-10-08 13:07:49 +0530

    added hello world

--- src/main/java/com/calc/project_calc/ProjectCalcApplication.java
+++ src/main/java/com/calc/project_calc/ProjectCalcApplication.java
@@ -1,13 +1,15 @@
 package com.calc.project_calc;
 
-import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ProjectCalcApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProjectCalcApplication.class, args);
+		//SpringApplication.run(ProjectCalcApplication.class, args);
+
+		System.out.println("HELLO");
 	}
 
 }
